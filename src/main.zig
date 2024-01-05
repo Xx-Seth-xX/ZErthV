701d83e8d1a7e6eb868400a6e78801e75b3a1626
Seth <53190848+Xx-Seth-xX@users.noreply.github.com>
2024-01-05 21:27:55 +0100

    Added moon

--- src/main.zig
+++ src/main.zig
@@ -16,8 +16,20 @@ const earth_r_au = earth_r * km_per_au;
 const sun_r = 700_000.0;
 const sun_r_au = sun_r * km_per_au;
 
-fn Vector3(x: f32, y: f32, z: f32) ray.Vector3 {
+inline fn vector2(x: f32, y: f32) ray.Vector3 {
+    return ray.Vector3{ .x = x, .y = y };
+}
+
+inline fn vector2FromAngle(mag: f32, phi: f32) ray.Vector2 {
+    return vector2(std.math.cos(phi) * mag, std.math.sin(phi) * mag);
+}
+
+inline fn vector3(x: f32, y: f32, z: f32) ray.Vector3 {
     return ray.Vector3{ .x = x, .y = y, .z = z };
+}
+
+inline fn vector3XYFromAngle(mag: f32, phi: f32) ray.Vector3 {
+    return vector3(std.math.cos(phi) * mag, std.math.sin(phi) * mag, 0.0);
 }
 
 fn updateCamera(camera: *ray.Camera3D, dt: f32) void {
@@ -57,9 +69,9 @@ fn updateCamera(camera: *ray.Camera3D, dt: f32) void {
 
 fn setUpCamera() ray.Camera3D {
     var camera = ray.Camera3D{};
-    camera.position = Vector3(-150.0, 0.0, 0.0);
-    camera.target = Vector3(0.0, 0.0, 0.0);
-    camera.up = Vector3(0.0, 0.0, 1.0);
+    camera.position = vector3(-150.0, 0.0, 0.0);
+    camera.target = vector3(0.0, 0.0, 0.0);
+    camera.up = vector3(0.0, 0.0, 1.0);
     camera.fovy = 45.0;
     camera.projection = ray.CAMERA_PERSPECTIVE;
     return camera;
@@ -84,11 +96,11 @@ const Astro = struct {
 
     fn init(r: f32, pos: ray.Vector3, axis: ray.Vector3, color: ray.Color, textu_: ?ray.Texture) Self {
         var model = ray.LoadModelFromMesh(ray.GenMeshSphere(r, rings, slices));
+
         // We calculate the rotation so the axis becomes the Z vector of the model
-        const z_v = Vector3(0.0, 0.0, 1.0);
+        const z_v = vector3(0.0, 0.0, 1.0);
         const rot = ray.Vector3Normalize(ray.Vector3CrossProduct(z_v, axis));
         const rot_angle = std.math.acos(ray.Vector3DotProduct(axis, z_v));
-        std.debug.print("rot_vector = {}, rot_angle = {}", .{ rot, rot_angle });
         model.transform = ray.MatrixRotate(rot, rot_angle);
         if (textu_) |textu| {
             model.materials[0].maps[ray.MATERIAL_MAP_DIFFUSE].texture = textu;
@@ -116,19 +128,35 @@ pub fn main() !void {
 
     var camera = setUpCamera();
 
-    const earth_axis = Vector3(0.0, std.math.sin(std.math.degreesToRadians(f32, 17.8)), std.math.cos(std.math.degreesToRadians(f32, 17.8)));
-    var earth = Astro.init(1.0, Vector3(30.0, 0.0, 0.0), earth_axis, ray.WHITE, try loadTexture("res/img/blue_marble.png"));
-    var sun = Astro.init(10.0, Vector3(0.0, 0.0, 0.0), Vector3(0.0, 0.0, 1.0), ray.RED, null);
+    const sun_earth_len: f32 = 30;
+    const earth_moon_len: f32 = 5;
+    const earth_axis = vector3(0.0, std.math.sin(std.math.degreesToRadians(f32, 17.8)), std.math.cos(std.math.degreesToRadians(f32, 17.8)));
+    var earth = Astro.init(1.0, vector3(sun_earth_len, 0.0, 0.0), earth_axis, ray.WHITE, try loadTexture("res/img/blue_marble.png"));
+    var sun = Astro.init(10.0, vector3(0.0, 0.0, 0.0), vector3(0.0, 0.0, 1.0), ray.YELLOW, null);
+    var moon = Astro.init(0.2, vector3(sun_earth_len + earth_moon_len, 0.0, 0.0), vector3(0.0, 0.0, 1.0), ray.GRAY, null);
+    var earth_phi: f32 = 0.0;
+    var moon_phi: f32 = 0.0;
+    var pause = false;
 
     while (!ray.WindowShouldClose()) {
         const dt = ray.GetFrameTime();
         updateCamera(&camera, dt);
+        if (ray.IsKeyPressed(ray.KEY_P)) {
+            pause = !pause;
+        }
+        if (!pause) {
+            earth_phi += dt / 4;
+            earth.pos = vector3XYFromAngle(sun_earth_len, earth_phi);
+            earth.rotateByAxis(dt * 100);
 
-        earth.rotateByAxis(dt);
+            moon_phi += dt * 4;
+            moon.pos = ray.Vector3Add(earth.pos, vector3XYFromAngle(earth_moon_len, moon_phi));
+        }
         ray.BeginDrawing();
-        ray.ClearBackground(ray.RAYWHITE);
+        ray.ClearBackground(ray.BLACK);
         ray.BeginMode3D(camera);
         earth.draw();
+        moon.draw();
         sun.draw();
         ray.EndMode3D();
         ray.EndDrawing();
